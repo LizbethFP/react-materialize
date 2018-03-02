24355861a3a9e709c08f18134cdf4f03dd2259b5
LizbethFP <lizbeth.fp@gmail.com>
2018-03-02 12:59:28 -0500

    react operativo

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import 'materialize/dist/css/materialize.css';
+// import 'materialize/dist/css/materialize.css';
+// import 'bootstrap/dist/css/bootstrap.css';
+// importar componente App de archivo Ap
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
